7fc2928054740919d02ad64501df6029a8160ed7
JamesTRoss1 <78627714+JamesTRoss1@users.noreply.github.com>
2022-11-06 13:17:35 -0700

    Update Pizza.java

--- Pizza.java
+++ Pizza.java
@@ -1,8 +1,8 @@
 public class Pizza implements java.io.Serializable
 {
-    private String[] toppings;
-    private String pizzaSize;
-    private String pizzaType;
+    public String[] toppings;
+    public String pizzaSize;
+    public String pizzaType;
 
     public Pizza(String[] toppings, String pizzaSize, String pizzaType)
     {
