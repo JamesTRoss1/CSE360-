39729299c67fa8347b0cf00bcaba7badf52d0481
JamesTRoss1 <78627714+JamesTRoss1@users.noreply.github.com>
2022-11-06 12:20:25 -0800

    Add files via upload
    Added getters and setters

--- Order.java
+++ Order.java
@@ -3,12 +3,12 @@ import java.lang.*;
 import java.time.*;
 
 public class Order implements  java.io.Serializable {
-    public int orderNumber;
-    public String asu;
-    public double price;
-    public Pizza pizza;
-    public LocalDateTime pickup;
-    public String status;
+    private int orderNumber;
+    private String asu;
+    private double price;
+    private Pizza pizza;
+    private LocalDateTime pickup;
+    private String status;
 
     public Order(int orderNumber, double price, String asu, Pizza pizza, int pickup) {
         this.orderNumber = orderNumber;
@@ -17,6 +17,54 @@ public class Order implements  java.io.Serializable {
         this.pizza = pizza;
         this.pickup = LocalDateTime.now().plusMinutes(pickup);
         this.status = "Processing";
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getAsu() {
+        return asu;
+    }
+
+    public void setAsu(String asu) {
+        this.asu = asu;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public LocalDateTime getPickup() {
+        return pickup;
+    }
+
+    public void setPickup(LocalDateTime pickup) {
+        this.pickup = pickup;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
 
--- Pizza.java
+++ Pizza.java
@@ -1,13 +1,37 @@
 public class Pizza implements java.io.Serializable
 {
-    public String[] toppings;
-    public String pizzaSize;
-    public String pizzaType;
+    private String[] toppings;
+    private String pizzaSize;
+    private String pizzaType;
 
     public Pizza(String[] toppings, String pizzaSize, String pizzaType)
     {
         this.toppings = toppings;
         this.pizzaSize = pizzaSize;
+        this.pizzaType = pizzaType;
+    }
+
+    public String[] getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(String[] toppings) {
+        this.toppings = toppings;
+    }
+
+    public String getPizzaSize() {
+        return pizzaSize;
+    }
+
+    public void setPizzaSize(String pizzaSize) {
+        this.pizzaSize = pizzaSize;
+    }
+
+    public String getPizzaType() {
+        return pizzaType;
+    }
+
+    public void setPizzaType(String pizzaType) {
         this.pizzaType = pizzaType;
     }
 }
